aae14cf3f218fd7beef21000cb80f6a6d4443341
mikef <mike.fullmore@propelplm.com>
2024-02-07 12:21:33 -0800

    pushing for now, continue lateer

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ module.exports = app.listen(process.env.PORT || 5001);
 /**
  * objects used in routes
  */
+const PimOneTwoFiveClient = require('./lib/client/PimOneTwoFiveClient');
+
+const ERROR_OBJ = { message: '', success: false };
+const SUCCESS_OBJ = { message: 'Request received', success: true };
 
 
 /**
@@ -22,4 +26,15 @@ module.exports = app.listen(process.env.PORT || 5001);
  */
 app.get('/', (req, res) => {
   res.status(200).send('Propel Upgrade server is running.');
+});
+
+app.post('/upgrade/package/pim/1.25', (req, res) => {
+  try {
+    new PimOneTwoFiveClient(req, res);
+    res.status(200).send(SUCCESS_OBJ);
+  } catch(error) {
+    ERROR_OBJ.message = error;
+    res.status(400).send(ERROR_OBJ);
+    console.error(ERROR_OBJ)
+  }
 });
